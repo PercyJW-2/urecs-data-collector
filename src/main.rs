617cb5f62af33915d44baa08f21c057bbf0ee488
PercyJW-2 <github@mail.joris-wachsmuth.de>
2025-05-07 14:44:49 +0200

    Continued to implement jetson data gathering started to implement urecs-firmware data gathering

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod network;
 
 use std::str::FromStr;
+use std::sync::{Arc, Mutex};
 use clap::{Parser, Subcommand};
 use subenum::subenum;
+use crate::network::{DataThread, ShutdownFn};
 
 #[derive(Parser, Debug)]
 struct Arguments {
@@ -66,15 +68,29 @@ enum Commands {
 
 fn main() {
     let args = Arguments::parse();
+    
+    let shutdown_funcs = Arc::new(Mutex::new(Vec::<network::ShutdownFn>::new()));
+    let shutdown_funcs_hook = shutdown_funcs.clone();
+    ctrlc::set_handler(move || {
+        println!("Shutting down...");
+        for func in shutdown_funcs_hook.lock().expect("Failed to lock the shutdown hook").iter() {
+            if let Err(err) = func() {
+                println!("Error: {}", err);
+            }
+        }
+        println!("Finished shutdown.");
+    }).expect("Error setting Ctrl-C handler");
+
+    let mut data_threads = Vec::new();
     println!("{:?}", args);
     match args.command {
         Commands::Single { address, source } => {
             match source {
                 Sources::Jetson { data_port, control_port } => {
-
+                    launch_jetson(&shutdown_funcs, &mut data_threads, address, data_port, control_port);
                 },
                 Sources::Firmware {} => {
-
+                    launch_firmware(&shutdown_funcs, &mut data_threads, address);
                 },
                 Sources::FastFirmware { data_port } => {
 
@@ -82,14 +98,50 @@ fn main() {
             }
         },
         Commands::Dual {urecs_address, jetson_address, jetson_data, firmware_type} => {
+            let jetson_data_port;
+            let jetson_control_port;
+            match jetson_data {
+                Jetson::Jetson { data_port, control_port } => {
+                    jetson_data_port = data_port;
+                    jetson_control_port = control_port;
+                }
+            }
+            launch_jetson(&shutdown_funcs, &mut data_threads, jetson_address, jetson_data_port, jetson_control_port);
             match firmware_type {
                 Firmware::Firmware {} => {
-
+                    launch_firmware(&shutdown_funcs, &mut data_threads, urecs_address);
                 },
                 Firmware::FastFirmware { data_port } => {
 
                 }
             }
+        }
+    }
+    for data_thread in data_threads {
+        data_thread.join().expect("DataThread join failed");
+    }
+}
+
+fn launch_firmware(shutdown_funcs: &Arc<Mutex<Vec<ShutdownFn>>>, data_threads: &mut Vec<DataThread>, address: String) {
+    match network::get_data_from_firmware(address) {
+        Ok((shutdown_func, data_thread)) => {
+            shutdown_funcs.lock().expect("Failed to lock the shutdown hook").push(shutdown_func);
+            data_threads.push(data_thread);
+        }
+        Err(error) => {
+            println!("Failed to setup Firmware networking: {}", error);
+        }
+    }
+}
+
+fn launch_jetson(shutdown_funcs: &Arc<Mutex<Vec<ShutdownFn>>>, data_threads: &mut Vec<DataThread>, jetson_address: String, jetson_data_port: u16, jetson_control_port: u16) {
+    match network::get_data_from_jetson(jetson_address, jetson_data_port, jetson_control_port) {
+        Ok((shutdown_func, data_thread)) => {
+            shutdown_funcs.lock().expect("Failed to lock the shutdown hook").push(shutdown_func);
+            data_threads.push(data_thread);
+        }
+        Err(error) => {
+            println!("Failed to setup Jetson networking: {}", error);
         }
     }
 }
--- src/network.rs
+++ src/network.rs
@@ -1,9 +1,98 @@
-use std::io;
-use std::io::Error;
-use std::net::UdpSocket;
+use std::io::{Write};
+use std::net::{TcpStream, UdpSocket};
+use std::thread;
+use std::time::Duration;
+use http_req::request::{Authentication, Method, Request};
+use http_req::uri::Uri;
+use serde::Deserialize;
+use anyhow::Result;
 
-fn get_data_from_jetson(address: String, data_port: u16, control_port: u16) -> Result<(), Error> {
+pub(crate) type ShutdownFn = Box<dyn Fn()->Result<()> + Send + Sync>;
+pub(crate) type DataThread = thread::JoinHandle<()>;
+
+pub(crate) fn get_data_from_jetson(address: String, data_port: u16, control_port: u16) -> Result<(ShutdownFn, DataThread)> {
     let socket = UdpSocket::bind(format!("0.0.0.0:{}", data_port))?;
     socket.connect(format!("{}:{}", address, data_port))?;
-    Ok(())
+    let data_thread = thread::spawn(move || {
+
+    });
+    Ok((
+        Box::new(move || {
+            println!("Shutting down Jetson Interface");
+            let mut control_connection = TcpStream::connect(format!("{}:{}", address, control_port))?;
+            control_connection.write("stop\n".as_bytes())?;
+            Ok(())
+        }),
+        data_thread
+    ))
+}
+
+#[derive(Debug, Deserialize)]
+struct NodeJetson {
+    #[serde(rename = "@maxPowerUsage")]
+    max_power_usage: f64,
+    #[serde(rename = "@architecture")]
+    architecture: String,
+    #[serde(rename = "@baseboardId")]
+    baseboard_id: String,
+    #[serde(rename = "@voltage")]
+    voltage: f64,
+    #[serde(rename = "@actualNodePowerUsage")]
+    actual_node_power_usage: f64,
+    #[serde(rename = "@actualPowerUsage")]
+    actual_power_usage: f64,
+    #[serde(rename = "@state")]
+    state: u8,
+    #[serde(rename = "@lastPowerState")]
+    last_power_state: u8,
+    #[serde(rename = "@defaultPowerState")]
+    default_power_state: u8,
+    #[serde(rename = "@rcuId")]
+    rcu_id: String,
+    #[serde(rename = "@health")]
+    health: String,
+    #[serde(rename = "@lastSensorUpdate")]
+    last_sensor_update: usize,
+    #[serde(rename = "@id")]
+    id: String,
+    #[serde(rename = "@present")]
+    present: bool,
+    #[serde(rename = "@forceRecovery")]
+    force_recovery: bool,
+    #[serde(rename = "@jetsonType")]
+    jetson_type: String,
+    #[serde(rename = "@baseboardPosition")]
+    baseboard_position: u8
+}
+
+pub(crate) fn get_data_from_firmware(address: String) -> Result<(ShutdownFn, DataThread)> {
+    // this is hideous, but I don't know a different option
+    let uri_str: &'static str = format!("https://{}/REST/node/RCU_0_BB_1_1", address).leak();
+    let uri = Uri::try_from(uri_str)?;
+    let data_thread = thread::spawn(move || {
+        let mut last_voltage:f64 = 0.0;
+        let mut last_power:f64 = 0.0;
+        loop {
+            let mut body = Vec::new();
+            let _ = Request::new(&uri)
+                .authentication(Authentication::basic("admin", "admin"))
+                .method(Method::GET)
+                .send(&mut body);
+            let body = String::from_utf8_lossy(&body);
+            let node: NodeJetson = quick_xml::de::from_str(&body).expect("Could not parse node XML");
+            if last_voltage != node.voltage || last_power != node.actual_power_usage { 
+                last_voltage = node.voltage;
+                last_power = node.actual_power_usage;
+                println!("Measurement: {}V, {}mW", node.voltage, node.actual_power_usage);
+            }
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+    Ok((
+        Box::new(move || {
+            println!("Shutting down Firmware Interface");
+            Ok(())
+        }),
+        data_thread
+    ))
 }
