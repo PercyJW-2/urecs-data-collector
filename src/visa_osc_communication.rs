25d3544d3df7194e2fb908854e50c807ec12f424
Joris Wachsmuth <joris.wachsmuth@uni-bielefeld.de>
2025-06-30 11:24:58 +0200

    switched to streaming data

--- src/visa_osc_communication.rs
+++ src/visa_osc_communication.rs
@@ -1,11 +1,12 @@
 use crate::{DataThread, ShutdownFn};
 use anyhow::Result;
 use std::ffi::CString;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 use std::thread;
 use std::time::SystemTime;
+use serde::Serialize;
 use visa_rs::enums::status::ErrorCode;
 use visa_rs::flags::AccessMode;
 use visa_rs::{io_to_vs_err, AsResourceManager, DefaultRM, Error, Instrument, TIMEOUT_IMMEDIATE};
@@ -19,7 +20,7 @@ fn setup_osc() -> Result<(Instrument, DefaultRM)> {
         println!("{:?}", item);
         item.and_then(|item| {
             let item_str = item.to_string_lossy();
-            if item_str.starts_with("USB") || item_str.starts_with("ETH") {
+            if item_str.starts_with("USB") || item_str.starts_with("ETH") || item_str.starts_with("TCPIP") {
                 possible_targets.push(item);
             }
             Ok(())
@@ -40,15 +41,20 @@ pub(crate) fn get_data_from_osc() -> Result<(ShutdownFn, DataThread)> {
     let running_clone = running.clone();
     
     let data_thread = thread::spawn(move || -> Result<()> {
-        let (mut dev, rm) = setup_osc()?;
+        let (mut dev, _rm) = setup_osc()?;
         let mut instrument = InstrumentWrapper::new(&mut dev)?;
         instrument.initialize()?;
         let mut last_time = SystemTime::now();
+        instrument.write_inst(b"CURVEStream?\n")?;
         while running.load(std::sync::atomic::Ordering::Relaxed) {
-            let result = instrument.write_read_inst_bin(b"CURve?\n")?;
-            println!("{}ms\t{} samples", last_time.elapsed()?.as_millis(), result.len());
+            //let result = instrument.write_read_inst_bin(b"CURve?\n")?;
+            let result = instrument.read_bin()?;
+            let combined = result.chunks_exact(2).map(|chunk| {
+                u16::from_le_bytes([chunk[0], chunk[1]])
+            }).collect::<Vec<u16>>();
             last_time = SystemTime::now();
         }
+        instrument.write_inst(b"WFMpre?\n")?;
         Ok(())
     });
     
@@ -62,8 +68,17 @@ pub(crate) fn get_data_from_osc() -> Result<(ShutdownFn, DataThread)> {
     ))
 }
 
+const READ_AMOUNT: usize = 250000;
+
 struct InstrumentWrapper<'inst> {
     reader: BufReader<&'inst mut Instrument>,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct OscMeasurement {
+    measurement_time: usize,
+    raw_value: u16
 }
 
 impl<'inst> InstrumentWrapper<'inst> {
@@ -81,17 +96,21 @@ impl<'inst> InstrumentWrapper<'inst> {
         println!("{}", resp);
 
         // reset instrument to default settings
-        self.write_inst(b"*RST\n")?;
+        //self.write_inst(b"*RST\n")?;
 
         // initialize instrument TODO: discuss settings commands without * can be seperated by ; symbols
         let setup_commands = [
             b"DATA INIT\n".as_slice(),
             b"DATA SNAP\n".as_slice(),
             b"DATA:START 1;STOP 10000\n",
-            b"DATA:ENCDG FAS\n"
+            b"DATA:ENCDG FAS\n",
+            b"DATA:WID 2\n",
+            b"DATA:RESO FULL\n",
+            b"DATA:SOU CH1\n",
         ];
 
-        self.write_inst_list(setup_commands.as_slice())?;
+        //self.write_inst_list(setup_commands.as_slice())?;
+        self.write_inst(b"DATA:WID 2\n")?;
         print!("{}", self.write_read_inst(b"DATa?\n")?);
         print!("{}", self.write_read_inst(b"WFMpre?\n")?);
         Ok(())
@@ -130,6 +149,18 @@ impl<'inst> InstrumentWrapper<'inst> {
         self.reader.get_mut().write_all(msg_buf).map_err(io_to_vs_err)?;
         let mut buf = Vec::new();
         self.reader.read_until(b'\n', &mut buf).map_err(io_to_vs_err)?;
+        Ok(buf)
+    }
+
+    fn read(&mut self) -> Result<String> {
+        let mut buf = String::new();
+        self.reader.read_line(&mut buf).map_err(io_to_vs_err)?;
+        Ok(buf)
+    }
+
+    fn read_bin(&mut self) -> Result<[u8; READ_AMOUNT]> {
+        let mut buf = [0; READ_AMOUNT];
+        self.reader.read_exact(&mut buf).map_err(io_to_vs_err)?;
         Ok(buf)
     }
     
